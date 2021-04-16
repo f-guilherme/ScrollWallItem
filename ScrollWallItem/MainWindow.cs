5b835c1efcfb2b964f118a1ea466461e5da70437
f-guilherme <77418461+f-guilherme@users.noreply.github.com>
2021-04-15 23:54:14 -0300

    Add files via upload

---
+++ ScrollWallItem/MainWindow.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GeodeExampleCSharp
+{
+    public partial class MainWindow : Form
+    {
+        public event EventHandler OnWallItemMove;
+        public ScrollWallItem swi;
+        bool isBigSteps;
+        public MainWindow()
+        {
+            InitializeComponent();
+            #region Scroll event
+            textBox1.MouseWheel += new MouseEventHandler(TextBox1_ScrollWheel);
+            textBox2.MouseWheel += new MouseEventHandler(textBox2_ScrollWheel);
+            textBox3.MouseWheel += new MouseEventHandler(textBox3_ScrollWheel);
+            textBox4.MouseWheel += new MouseEventHandler(textBox4_ScrollWheel);
+            #endregion
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            swi = new ScrollWallItem(this);
+            TopMost = true;
+        }
+
+        #region textbox1
+        private void TextBox1_ScrollWheel(object sender, MouseEventArgs e)
+        {
+            HandledMouseEventArgs handledArgs = e as HandledMouseEventArgs;
+            handledArgs.Handled = true;
+
+            if (handledArgs.Delta < 0)
+            {
+                if (isBigSteps == false)
+                    swi.w1++;
+                else
+                    swi.w1 += 10;
+            }
+
+            else
+            {
+                if (isBigSteps == false)
+                    swi.w1--;
+                else
+                    swi.w1 -= 10;
+            }
+
+            textBox1.Text = swi.w1.ToString();
+        }
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            OnWallItemMove?.Invoke(this, null);
+            label6.Text = swi.Concat();
+        }
+        #endregion
+
+        #region textbox2
+        private void textBox2_ScrollWheel(object sender, MouseEventArgs e)
+        {
+            HandledMouseEventArgs handledArgs = e as HandledMouseEventArgs;
+            handledArgs.Handled = true;
+
+            if (handledArgs.Delta < 0)
+            {
+                if (isBigSteps == false)
+                    swi.w2++;
+                else
+                    swi.w2 += 10;
+            }
+
+            else
+            {
+                if (isBigSteps == false)
+                    swi.w2--;
+                else
+                    swi.w2 -= 10;
+            }
+            textBox2.Text = swi.w2.ToString();
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            OnWallItemMove?.Invoke(this, null);
+            label6.Text = swi.Concat();
+        }
+        #endregion
+
+        #region textbox3
+        private void textBox3_ScrollWheel(object sender, MouseEventArgs e)
+        {
+            HandledMouseEventArgs handledArgs = e as HandledMouseEventArgs;
+            handledArgs.Handled = true;
+
+            if (handledArgs.Delta < 0)
+            {
+                if (isBigSteps == false)
+                    swi.l1++;
+                else
+                    swi.l1 += 10;
+            }
+
+            else
+            {
+                if (isBigSteps == false)
+                    swi.l1--;
+                else
+                    swi.l1 -= 10;
+            }
+
+            textBox3.Text = swi.l1.ToString();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            OnWallItemMove?.Invoke(this, null);
+            label6.Text = swi.Concat();
+        }
+        #endregion
+
+        #region textbox4
+        private void textBox4_ScrollWheel(object sender, MouseEventArgs e)
+        {
+            HandledMouseEventArgs handledArgs = e as HandledMouseEventArgs;
+            handledArgs.Handled = true;
+
+
+            if (handledArgs.Delta < 0)
+            {
+                if (isBigSteps == false)
+                    swi.l2++;
+                else
+                    swi.l2 += 10;
+            }
+
+            else
+            {
+                if (isBigSteps == false)
+                    swi.l2--;
+                else
+                    swi.l2 -= 10;
+            }
+
+            textBox4.Text = swi.l2.ToString();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            OnWallItemMove?.Invoke(this, null);
+            label6.Text = swi.Concat();
+        }
+        #endregion
+
+        #region rotate
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (swi.rotation == "l") swi.rotation = "r";
+            else swi.rotation = "l";
+
+            OnWallItemMove.Invoke(this, null);
+        }
+        #endregion
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            isBigSteps = !isBigSteps;
+        }
+    }
+}
---
+++ ScrollWallItem/ScrollWallItem.cs
@@ -0,0 +1,79 @@
+﻿using Geode.Extension;
+using Geode.Network;
+using System;
+
+namespace GeodeExampleCSharp
+{
+    [Module("ScrollWallItem", "Arckmed", "Use your scroll wheel to move wall items.")]
+    public class ScrollWallItem : GService
+    {
+        public MainWindow MainWindowParent;
+        public string itemCoords { get; private set; }
+        public string rotation { get; set; }
+        public int itemId { get; private set; }
+        public int w1 { get; set; }
+        public int w2 { get; set; }
+        public int l1 { get; set; }
+        public int l2 { get; set; }
+
+        public ScrollWallItem(MainWindow MainWindowParent)
+        {
+            this.MainWindowParent = MainWindowParent;
+            this.MainWindowParent.OnWallItemMove += On_WallItemMove;
+        }
+
+        [OutDataCapture("MoveWallItem")]
+        void OnMoveWallItem(DataInterceptedEventArgs e)
+        {
+            itemId = e.Packet.ReadInt32();
+            itemCoords = e.Packet.ReadUTF8();
+            GetCoordsAndRotation(itemCoords);
+        }
+
+        public void GetCoordsAndRotation(string coords)
+        {
+            //structure: {s:":w=5,0 l=11,42 r"}
+            coords = coords.Substring(3, coords.Length - 3);
+            w1 = int.Parse(coords.Substring(0, coords.IndexOf(' ')).Split(new char[]
+            {
+                ','
+            })[0]);
+            w2 = int.Parse(coords.Substring(0, coords.IndexOf(' ')).Split(new char[]
+            {
+                ','
+            })[1]);
+            coords = coords.Substring(this.w1.ToString().Length + this.w2.ToString().Length + 4);
+            l1 = int.Parse(coords.Substring(0, coords.IndexOf(' ')).Split(new char[]
+            {
+                ','
+            })[0]);
+            l2 = int.Parse(coords.Substring(0, coords.IndexOf(' ')).Split(new char[]
+            {
+                ','
+            })[1]);
+            rotation = coords.Substring(coords.Length - 1);
+        }
+
+        public async void On_WallItemMove(object sender, EventArgs e)
+        {
+            await SendToServerAsync(Out.MoveWallItem, itemId, Concat());
+        }
+
+        public string Concat()
+        {
+            return string.Concat(new string[]
+            {
+                    ":w=",
+                    w1.ToString(),
+                    ",",
+                    w2.ToString(),
+                    " l=",
+                    l1.ToString(),
+                    ",",
+                    l2.ToString(),
+                    " ",
+                    rotation
+            });
+        }
+    }
+}
